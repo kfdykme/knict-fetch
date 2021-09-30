1e3efd790ba3a3e7e047641c85b526c117c38f77
kfdykme <wim.k.f@live.com>
2021-10-01 00:23:16 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import { FetchClientBuilder } from './client/FetchClientBuilder'
 import { FetchDemoService } from './demo/FetchDemoService'
 import * as HttpMethod from './methods/HttpMethod'
 import { Knict } from 'knict'
+import { AxiosResponse } from 'axios'
 
 export {
     Knict,
     FetchDemoService,
     FetchClientBuilder,
-    HttpMethod
+    HttpMethod,
+    AxiosResponse
 }
