96c64d339efcbbc6b1242ca4c3f6f933e9e5a0a4
kfdykme <wim.k.f@live.com>
2021-10-01 04:53:17 +0800

    update

--- src/client/FetchClientBuilder.ts
+++ src/client/FetchClientBuilder.ts
@@ -1,15 +1,19 @@
 import { KnictBasicClientBuilder } from 'knict'
-import axios from 'axios';
-
-
+import * as HttpMethod from '../methods/HttpMethod'
+import axios, { AxiosInstance } from 'axios';
  
 
 export class FetchClientBuilder extends KnictBasicClientBuilder {
 
     private baseUrl_ = ''
-
+    private axios_: AxiosInstance = axios
     baseUrl(url: string) {
         this.baseUrl_ = url
+        return this
+    }
+
+    axios(axiosInstanc: AxiosInstance) {
+        this.axios_ = axiosInstanc
         return this
     }
 
@@ -17,15 +21,56 @@ export class FetchClientBuilder extends KnictBasicClientBuilder {
         return k.data && k.data.path
     }
 
-    build(k: any): any {
-        if (this.baseUrl_ === '') {
-            throw new Error(`FetchClientBuilder Error: you need set a base url by FetchClientBuilderInstance.baseUrl(url)`)
-        }
-        console.info('Build Fetch', k)
+    hasPostData(k: any) {
+        return k.data && k.data.post
+    }
 
+    isPost(k: any) {
+        return k.http && k.http.method === 'POST'
+    }
+
+    isGet(k: any) {
+        return k.http && k.http.method === 'GET'
+    }
+
+    handlePost(k: any) {
+
+        const url = this.baseUrl_ + k.url
+        console.info('handlePost', url)
+        if (this.hasPostData(k)) {
+            const post = k.data.post
+            for (let x in post) {
+                const postx = k.args[post[x]]
+                if (!k.http.data) k.http.data = new Object()
+                k.http.data[x] = postx
+            }
+        }
+
+        if (k.http.type === HttpMethod.PostType.urlencoded) {
+            // convert as urlencoded post data
+            const res = []
+            const data: any = k.http.data
+            for (let x in data) {
+                res.push(`${x}=${data[x]}`)
+            }
+            k.http.data = res.join('&')
+        }
+
+
+        console.info('handlePost', k.http.data)
+        return this.axios_({
+            method: 'post',
+            url: url,
+            data: k.http.data
+        })
+    }
+
+    handleGet(k: any) {
 
         // resolve Get path
-        
+
+        const url = this.baseUrl_ + k.url
+        console.info('handleGet', url)
         if (this.hasPath(k)) {
             const path = k.data.path
             for (let x in path) {
@@ -34,12 +79,22 @@ export class FetchClientBuilder extends KnictBasicClientBuilder {
                 k.url = k.url.replace(reg, pathx)
             }
         }
+        return this.axios_.get(url)
+    }
 
+    build(k: any): any {
+        if (this.baseUrl_ === '') {
+            throw new Error(`FetchClientBuilder Error: you need set a base url by FetchClientBuilderInstance.baseUrl(url)`)
+        }
+        console.info('Build Fetch', k)
 
         return (() => {
-            const url = this.baseUrl_ + k.url
-            console.info('Fetch', url)
-            return axios.get(url)
+            if (this.isPost(k)) {
+                return this.handlePost(k)
+            } else if (this.isGet(k)) {
+                return this.handleGet(k)
+            }
+            return Promise.reject(new Error('Unsupport'))
 
         })()
     }
--- src/methods/HttpMethod.ts
+++ src/methods/HttpMethod.ts
@@ -28,7 +28,12 @@ function GET(url: string) {
     }
 }
 
-function POST(url: string) {
+enum PostType {
+    urlencoded = 'x-www-form-urlencoded',
+    // TODO: 
+}
+
+function POST(url: string, type: PostType = PostType.urlencoded) {
     logger.log('Knict POST(): evaluated')
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         let targetMethod = target[propertyKey]
@@ -38,13 +43,38 @@ function POST(url: string) {
                 url: url,
                 name: propertyKey,
                 http: {
-                    method: 'POST'
+                    method: 'POST',
+                    type: type,
+                    data: {
+
+                    }
                 }
             }
         }
         logger.log('Knict POST(): called', target, propertyKey, descriptor)
     }
 }
+
+function PostData(name: string) {
+    logger.log('Knict PostData() : evaluated')
+    return function (target: any, propertyKey: string | symbol, parameterIndex: number) {
+        let targetMethod = target[propertyKey]
+        if (targetMethod !== undefined && targetMethod instanceof Function) {
+            targetMethod.knict = {
+                ...targetMethod.knict
+            }
+            if (targetMethod.knict.data == undefined) {
+                targetMethod.knict.data = new Object()
+            }
+            if (targetMethod.knict.data.post == undefined) {
+                targetMethod.knict.data.post = new Object()
+            }
+            targetMethod.knict.data.post[name] = parameterIndex
+        }
+        console.log('Knict PostData(): called', target, propertyKey, parameterIndex)
+    }
+}
+
 
 function Path(path: string) {
     logger.log('Knict Path() : evaluated')
@@ -67,4 +97,13 @@ function Path(path: string) {
 }
 
 const OnUnsupport = (() => Promise.reject('Unsupport'))
-export { GET, Path, POST, OnUnsupport }
+
+
+declare interface Response<T = any>  {
+    data: T;
+    status: number;
+    statusText: string;
+    headers: any; 
+    request?: any;
+  }
+export { GET, Path, POST, PostData, OnUnsupport, Response, PostType}
